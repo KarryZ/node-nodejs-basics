bb783b53b4b51f6a748ff1c0f24a50b3a00424b8
KarryZ <katejidkova@gmail.com>
2022-06-05 16:38:47 +0300

    child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,12 @@
+import { fork } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork('./src/cp/files/script.js', args);
+    child.send(process.stdin)
+    child.on('message', data => {
+        process.stdout.pipe(data);
+    });
 };
+
+const arr = process.argv.slice(2);
+spawnChildProcess(arr);
